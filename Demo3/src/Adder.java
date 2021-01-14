f8191204ec2adf04ee6e6f63d5ff4ae26bfbedab
David Lash <dlash02@gmail.com>
2021-01-13 19:13:55 -0600

    update class 2

---
+++ Demo3/src/Adder.java
@@ -0,0 +1,12 @@
+public class Adder {
+    int x;
+    int y;
+
+    public Adder(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getSum(){
+        return x + y;
+    }
+}
