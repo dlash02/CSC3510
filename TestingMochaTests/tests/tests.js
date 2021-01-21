0a623990c076d158f1040eabdf36cc1e8621a2c6
David Lash <dlash02@gmail.com>
2021-01-21 09:32:46 -0600

    Update before class

---
+++ TestingMochaTests/tests/tests.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('file to be tested', function() => {
+    context("function to be tested"), function() => {
+        it( "should do something", function() => {
+            asser.equal( 1, 1);
+
+        })
+    }
+})
